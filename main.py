de2929817617ea556ed3130f18a2e5e86ea61404
Gruppo Utenti Linux Livorno <gulligle@gmail.com>
2017-06-19 20:12:49 +0200

    Rimossa scrittura dei log su file in favore del database SQLite

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 import requests
 import signal
 import time
+import os.path
+import sqlite3
 
 # Gestione interruzione script
 run = True
@@ -14,15 +16,49 @@ def handler_stop_signals(signum, frame):
 
 signal.signal(signal.SIGINT, handler_stop_signals)
 signal.signal(signal.SIGTERM, handler_stop_signals)
-# Fine
+
+# Creazione dei database
+dbFile = 'logBase.sqlite'
+dbEsiste = os.path.isfile(dbFile)
+connDB = sqlite3.connect(dbFile)
+cursDB = connDB.cursor()
+
+if not dbEsiste:
+    cursDB.execute(
+        '''
+            CREATE TABLE `rawUpdate` (
+                `id`            INTEGER NOT NULL UNIQUE,
+                `unixTime`      INTEGER NOT NULL,
+                `urlTg`         TEXT NOT NULL,
+                `updateText`    TEXT NOT NULL
+            );
+        '''
+    )
+    connDB.commit()
+    for rowInt in range(1, 11):
+        cursDB.execute(
+            '''
+            INSERT INTO `rawUpdate` (`id`, `unixTime`, `urlTg`, `updateText`)
+            VALUES (?, ?, ?, ?);
+            ''',
+            (rowInt, rowInt, str(rowInt), str(rowInt))
+        )
+    connDB.commit()
+
+rawUpdateCyc = 1
 
 
 token = ""
 apiUrl = "https://api.telegram.org/bot"
 timeout = 15
+try:
+    offsetFile = open('offset', 'r')
+    offset = int(offsetFile.read())
+    offsetFile.close()
+except FileNotFoundError:
+    offset = -1
 
 urlRichiesta = apiUrl + token + '/getMe'
-print(urlRichiesta)
 infoBot = requests.get(urlRichiesta)
 infoBotDict = json.loads(infoBot.text)
 botUsername = infoBotDict['result']['username']
@@ -32,24 +68,32 @@ def nuovaRichiesta(urlRichiesta):
     try:
         risposta = requests.get(urlRichiesta)
     except requests.exceptions.ConnectionError:
-        print('Errore di connessione')
+        print(str(time.ctime()) + ' Errore di connessione')
         time.sleep(60)
         risposta = nuovaRichiesta(urlRichiesta)
 
+    global rawUpdateCyc
+    if rawUpdateCyc > 10:
+        rawUpdateCyc = 1
+    cursDB.execute(
+        '''
+        UPDATE `rawUpdate`
+        SET `unixTime`=?, `urlTg`=?, `updateText`=?
+        WHERE `id`=?;
+        ''',
+        (int(time.time()), str(urlRichiesta), str(risposta.text),
+            int(rawUpdateCyc))
+    )
+    connDB.commit()
+    rawUpdateCyc += 1
+
     return risposta
-
-
-def richiesteLog(rispostaText):
-    richiesteLogFile = open('richieste.log', 'a')
-    richiesteLogFile.write(rispostaText + '\n')
-    richiesteLogFile.close()
 
 
 def sendMessage(chatId, text):
     urlRichiesta = apiUrl + token + '/sendMessage' \
         '?chat_id=' + chatId + '&text=' + text
-    print(urlRichiesta)
-    richiesteLog(nuovaRichiesta(urlRichiesta).text)
+    nuovaRichiesta(urlRichiesta)
 
 
 def estrNomeUser(memberDict):
@@ -68,31 +112,23 @@ def verComando(testo, comando):
     return amm
 
 
-while run:
-    try:
-        offsetFile = open('offset', 'r')
-        offset = int(offsetFile.read())+1
+while True:
+    if not run:
+        offsetFile = open('offset', 'w')
+        offsetFile.write(str(offset))
         offsetFile.close()
-    except FileNotFoundError:
-        offset = -1
+
+        cursDB.close()
+        connDB.close()
+
+        break
 
     urlRichiesta = apiUrl + token + '/getUpdates' \
         + '?offset=' + str(offset) + '&timeout=' + str(timeout)
-    print(urlRichiesta)
-    risposta = nuovaRichiesta(urlRichiesta)
-    rispostaDict = json.loads(risposta.text)
+    rispostaDict = json.loads(nuovaRichiesta(urlRichiesta).text)
 
     for result in rispostaDict['result']:
-        updateId = str(result['update_id'])
-
-        updateLogFile = open('update.log', 'a')
-        updateLogFile.write('UPDATE N° ' + updateId + '\n')
-        updateLogFile.write(json.dumps(result, indent=4) + '\n')
-        updateLogFile.close()
-
-        offsetFile = open('offset', 'w')
-        offsetFile.write(updateId)
-        offsetFile.close()
+        offset = int(result['update_id'])+1
 
         if 'message' in result:
             chatId = str(result['message']['chat']['id'])
